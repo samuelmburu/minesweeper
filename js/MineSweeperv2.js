ba15f63c89e4357f045d6d75ccee15aeeddc0559
Samuel Mburu <mburu.samuel@gmail.com>
2015-03-08 15:15:23 -0700

    fixed scoring bugs

--- js/MineSweeperv2.js
+++ js/MineSweeperv2.js
@@ -21,10 +21,18 @@ var MineSweeper = (function () {
 
         // get a new board position if we don't have a valid position and the
         // position returned is not a mine
-        while(nextMinePosition != -1 && board[nextMinePosition] !== -1) {
+        while(nextMinePosition === -1) {
             // we are using the modulator here to vary the new location if
             // the last one generated was already a mine
-            nextMinePosition = Math.random() * (height * width - modulator) + modulator;
+            nextMinePosition = Math.floor(Math.random() * (height * width - modulator) + modulator);
+
+            // verify next position isn't a mine already
+            if(board[nextMinePosition] === -1) {
+                // set nextMinePosition to an invalid value
+                nextMinePosition = -1;
+                // increment modulator so we get a 'better' number
+                modulator++;
+            }
         }
         return nextMinePosition;
     }
@@ -39,14 +47,14 @@ var MineSweeper = (function () {
         // while there are more items in this array of calculations
         while(len--) {
             // get index of next cell we need to score
-            nextCell = index + cellProximityCalculations[cellPos][len];
+            nextCell = parseInt(index, 10) + parseInt(cellProximityCalculations[cellPos][len],  10);
             // if nextCell location is on the board
-            if (nextCell> 0 && nextCell <= board.length -1) {
+            if (nextCell >= 0 && nextCell <= board.length -1) {
                 adjacentCells += nextCell + ",";
             }
         }
         // return the adjacentCell string as an array after we remove the last comma
-        return adjacentCells.substring(-1).split(",");
+        return adjacentCells.slice(0, -1).split(",");
     }
 
     function _placeMines () {
@@ -57,8 +65,23 @@ var MineSweeper = (function () {
         }
     }
 
+    function _outputBoard() {
+        //deompose board
+        var tempBoard = [],
+            width = board.width,
+            height = board.height;
+
+        for (var i = 0; i < height; i++) {
+            tempBoard.push(board.slice(i * width, i * width + width).join(' ').replace(/-1/g,'X'));
+        }
+
+        //display board
+        console.log(tempBoard.join('\n'));
+    }
+
     function _scoreBoard() {
         var currenctCell,
+            currentCellVal,
             adjacentCellArray,
             cellArrayLengthCounter;
 
@@ -66,25 +89,27 @@ var MineSweeper = (function () {
         // to find all the mines and score all their adjacent cells.
         for(var minePos in board) {
             adjacentCellArray = _getAdjacentCellArray(minePos);
-            cellArrayLengthCounter = adjacentCellArray.length - 1;
+            cellArrayLengthCounter = adjacentCellArray.length;
 
             while (cellArrayLengthCounter--) {
-                currenctCell = board[adjacentCellArray[cellArrayLengthCounter]];
+                currenctCell = adjacentCellArray[cellArrayLengthCounter];
+                currentCellVal = board[currenctCell];
                 // if cell does not have a mine then update the scoring
-                if (currenctCell !== -1) {
+                if (currentCellVal !== -1) {
                     // if the currentCell doesn't have a score then set it to one
                     // otherwise increment the existing cell
-                    currenctCell = (currenctCell === undefined)? 1 : currenctCell++;
+                    currentCellVal = (currentCellVal === undefined) ? 0 : currentCellVal;
+                    board[currenctCell] = ++currentCellVal;
                 }
             }
         }
     }
     return {
-        init: function(height, width, mineCount) {
-            var boardLen = height * width;
-            height = height;
-            width = width;
-            mineCount = mineCount;
+        init: function(h, w, m) {
+            var boardLen = h * w;
+            height = h;
+            width = w;
+            mineCount = m;
 
             // set up cellProximityCalculations
             cellProximityCalculations = {
@@ -101,6 +126,8 @@ var MineSweeper = (function () {
 
             // set up board
             board[boardLen] = undefined;
+            board['height'] = height;
+            board['width'] = width;
 
             // place mines on board
             _placeMines();
@@ -108,17 +135,7 @@ var MineSweeper = (function () {
             // score board
             _scoreBoard();
         },
-        outputBoard: function () {
-            //deompose board
-            var tempBoard = [];
-
-            for (var i = 0; i < height; i++) {
-                tempBoard.push(board.slice(i * width, i * width + width).join(' ').replace(/-1/g,'X'));
-            }
-
-            //display board
-            console.log(tempBoard.join('\n'));
-        }
+        outputBoard: _outputBoard
     };
 }());
 
@@ -126,7 +143,5 @@ var MineSweeper = (function () {
 
 
 console.clear();
-var m = MineSweeper.init(3,5,4);
-m.init();
-m.scoreBoard();
-m.outputBoard();
+MineSweeper.init(3,5,4);
+MineSweeper.outputBoard();
